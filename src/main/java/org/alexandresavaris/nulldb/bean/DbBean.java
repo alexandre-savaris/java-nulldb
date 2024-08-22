b8a0d81c2532bfabb08e9bfee8edf6f3854790d1
Alexandre Savaris <alexandre.savaris@gmail.com>
2024-08-22 10:52:45 -0300

    Loading already existing database logs into the Memtable.

--- src/main/java/org/alexandresavaris/nulldb/bean/DbBean.java
+++ src/main/java/org/alexandresavaris/nulldb/bean/DbBean.java
@@ -1,10 +1,13 @@
 package org.alexandresavaris.nulldb.bean;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.alexandresavaris.nulldb.pojo.Entry;
 import org.alexandresavaris.nulldb.util.ByteArrayComparator;
 import org.alexandresavaris.nulldb.util.Utils;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 // For managing on-disk and in-memory database structures.
@@ -23,6 +26,7 @@ public class DbBean {
         this.dbFilePath = Utils.getDatabasePath(dbPath, dbFileName);
 
         this.memTable = new TreeMap<>(new ByteArrayComparator());
+        this.hydrate();
     }
 
     public String getDbPath() {
@@ -43,5 +47,18 @@ public class DbBean {
     public TreeMap<byte[], byte[]> getMemTable() {
 
         return memTable;
+    }
+
+    // Load the Memtable from the on-disk database log.
+    private void hydrate() throws IOException {
+        // For entries deserialization.
+        ObjectMapper mapper = new ObjectMapper();
+
+        Scanner scanner = new Scanner(this.dbFilePath);
+        while (scanner.hasNextLine()) {
+            String jsonEntry = scanner.nextLine();
+            Entry entry = mapper.readValue(jsonEntry, Entry.class);
+            this.memTable.put(entry.getKey(), entry.getValue());
+        }
     }
 }
